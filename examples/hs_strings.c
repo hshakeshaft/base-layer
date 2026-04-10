f1fd31ad6442ae4279e1612e6fc45dc30dd2dcfe
hshakeshaft <henry.shakeshaft@live.co.uk>
2026-04-10 17:06:44 +0100

    hs_strings: create initial API for working with strings

--- examples/hs_strings.c
+++ examples/hs_strings.c
@@ -5,9 +5,23 @@
 #include <stdio.h>
 
 int main(void) {
+    char *cstr1;
+    hss_String str1;
+    hss_IAllocator *allocator;
+
+    allocator = &hss_default_allocator;
+
     printf("hs_strings version: %s\n", hss_api_version());
 
-    printf("hss_strlen %i\n", hss_string_len("foo"));
+    cstr1 = "foo";
+    printf("\"%s\"\n", cstr1);
+    printf("  hss_string_len %i\n", hss_string_len(cstr1));
+
+    hss_string_create(allocator, &str1, 12);
+    hss_string_copy("Hello, World", str1);
+    printf("\"%s\"\n", str1);
+    printf("  hss_string_len %i\n", hss_string_len(str1));
+    hss_string_destroy(&str1);
 
     return 0;
 }
